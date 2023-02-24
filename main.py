5caaaf2eda0cba7c17c9b9767587da7262a49b6b
diogo2806 <diogo_2806@hotmail.com>
2023-02-24 00:31:25 +0000

    update
    Signed-off-by: diogo2806 <diogo_2806@hotmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import openai
 from dotenv import load_dotenv
 import os
+from flask import Flask, request, jsonify
 
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def ask_openai(prompt, model, temperature=0.5, max_tokens=2048, top_p=1, frequency_penalty=0, presence_penalty=0):
+app = Flask(__name__, static_folder=".", static_url_path="")
+conversations = {}
+
+def ask_openai(prompt, model, temperature=0.5, max_tokens=2048, top_p=1, frequency_penalty=0, presence_penalty=0, conversation_id=None):
     completions = openai.Completion.create(
         engine=model,
         prompt=prompt,
@@ -15,14 +19,35 @@ def ask_openai(prompt, model, temperature=0.5, max_tokens=2048, top_p=1, frequen
         top_p=top_p,
         frequency_penalty=frequency_penalty,
         presence_penalty=presence_penalty,
+        prompt_suffix=f"\nConversation ID: {conversation_id}" if conversation_id else None,
+        stop=["\n"],
     )
     message = completions.choices[0].text
     return message.strip()
 
-def main():
-    prompt = "para que serve o chatgpt?"
-    response = ask_openai(prompt, "text-davinci-003")
-    print(response)
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    message = request.json.get("message")
+    conversation_id = request.json.get("conversation_id")
+    
+    if conversation_id is None:
+        conversation_id = str(len(conversations))
+        conversations[conversation_id] = {"prompt": "", "response": ""}
+
+    conversations[conversation_id]["prompt"] += f"{message}\n"
+    response = ask_openai(
+        prompt=conversations[conversation_id]["prompt"], 
+        model="text-davinci-002", 
+        max_tokens=200, 
+        conversation_id=conversation_id
+    )
+    conversations[conversation_id]["response"] += f"{response}\n"
+    
+    return jsonify({"answer": response, "conversation_id": conversation_id})
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
